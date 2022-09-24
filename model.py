02d2523c302948a1b57e844c6e0182309edb257f
howerdni <chengjie_ni@163.com>
2022-09-24 14:13:32 +0800

    Add files via upload

---
+++ model.py
@@ -0,0 +1,94 @@
+#----------------------------------------------------------------------------#
+# Models.
+#----------------------------------------------------------------------------#
+import json
+import dateutil.parser
+import babel
+from flask import Flask, render_template, request, Response, flash, redirect, url_for, abort, jsonify
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+import logging
+from logging import Formatter, FileHandler
+from forms import *
+from flask_migrate import Migrate
+
+from datetime import datetime
+import re
+
+app = Flask(__name__)
+moment = Moment(app)
+db = SQLAlchemy(app)
+
+class Genre(db.Model):
+    __tablename__ = 'Genre'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+
+artist_genre_table = db.Table('artist_genre_table',
+    db.Column('genre_id', db.Integer, db.ForeignKey('Genre.id'), primary_key=True),
+    db.Column('artist_id', db.Integer, db.ForeignKey('Artist.id'), primary_key=True)
+)
+
+venue_genre_table = db.Table('venue_genre_table',
+    db.Column('genre_id', db.Integer, db.ForeignKey('Genre.id'), primary_key=True),
+    db.Column('venue_id', db.Integer, db.ForeignKey('Venue.id'), primary_key=True)
+)
+
+
+class Venue(db.Model):
+    __tablename__ = 'Venue'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    address = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+
+    genres = db.relationship('Genre', secondary=venue_genre_table, backref=db.backref('venues'))
+    
+    website = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean, default=False)
+    seeking_description = db.Column(db.String(120))
+
+    
+    shows = db.relationship('Show', backref='venue', lazy=True)    
+
+
+
+class Artist(db.Model):
+    __tablename__ = 'Artist'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+
+
+    genres = db.relationship('Genre', secondary=artist_genre_table, backref=db.backref('artists'))
+
+    website = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean, default=False)
+    seeking_description = db.Column(db.String(120))
+
+
+    shows = db.relationship('Show', backref='artist', lazy=True)  
+
+
+
+
+class Show(db.Model):
+    __tablename__ = 'Show'
+
+    id = db.Column(db.Integer, primary_key=True)
+    start_time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)    # Start time required field
+
+    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)   # Foreign key is the tablename.pk
+    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
